f15ff2d3730bb8aed2fc213353b8333741a013f4
HaoyangLiu <lhy.0318@163.com>
2020-05-20 14:16:12 +0800

    add the mock package

---
+++ contracts/mock/MockRelayerHub.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.6.4;
+
+import "../interface/IRelayerHub.sol";
+
+contract MockRelayerHub is IRelayerHub {
+
+  function isRelayer(address) external override view returns (bool){
+    return true;
+  }
+}
