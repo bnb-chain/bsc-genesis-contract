f221f684a03648faf1d02c2dc8afdae301732996
HaoyangLiu <lhy.0318@163.com>
2020-06-01 19:20:00 +0800

    implement unbind

--- truffle-config.js
+++ truffle-config.js
@@ -90,7 +90,7 @@ module.exports = {
       settings: {          // See the solidity docs for advice about optimization and evmVersion
        optimizer: {
          enabled: true,
-         runs: 200
+         runs: 50
        }
       }
     }
