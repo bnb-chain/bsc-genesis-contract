a281daa398e0d55d29ee9664b8db579fc4814e60
Roshan <48975233+loverush@users.noreply.github.com>
2022-03-16 14:06:50 +0800

    [WIP]Fast Finality: reward distribution and slash parts (#8)
    * [WIP]Fast Finality: reward distribution and slash parts
    
    * Update codes
    
    * Fix review comments and update test
    
    * Fix code errors related contract SystemReward and BSCValidatorSet
    
    * Fix code errors related invalid opcode and add new api
    
    * Optimize gasused of updateValidator and update test
    
    * Optimize gasused of updateValidator and update test
    
    * Minor error fixed
    
    * Fix review comments and update scripts
    
    * Fix init issue
    
    * Fix external view issue and update test
    
    * Fix review comments

--- contracts/TendermintLightClient.sol
+++ contracts/TendermintLightClient.sol
@@ -7,13 +7,13 @@ import "./interface/ISystemReward.sol";
 import "./interface/IParamSubscriber.sol";
 import "./System.sol";
 
-contract TendermintLightClient is ILightClient, System, IParamSubscriber{
+contract TendermintLightClient is ILightClient, System, IParamSubscriber {
 
   struct ConsensusState {
-    uint64  preValidatorSetChangeHeight;
+    uint64 preValidatorSetChangeHeight;
     bytes32 appHash;
     bytes32 curValidatorSetHash;
-    bytes   nextValidatorSet;
+    bytes nextValidatorSet;
   }
 
   mapping(uint64 => ConsensusState) public lightClientConsensusStates;
@@ -23,7 +23,7 @@ contract TendermintLightClient is ILightClient, System, IParamSubscriber{
   bytes32 public chainID;
 
   bytes constant public INIT_CONSENSUS_STATE_BYTES = hex"42696e616e63652d436861696e2d4e696c650000000000000000000000000000000000000000000229eca254b3859bffefaf85f4c95da9fbd26527766b784272789c30ec56b380b6eb96442aaab207bc59978ba3dd477690f5c5872334fc39e627723daa97e441e88ba4515150ec3182bc82593df36f8abb25a619187fcfab7e552b94e64ed2deed000000e8d4a51000";
-  uint256 constant public INIT_REWARD_FOR_VALIDATOR_SER_CHANGE  = 1e16;
+  uint256 constant public INIT_REWARD_FOR_VALIDATOR_SER_CHANGE = 1e16;
   uint256 public rewardForValidatorSetChange;
 
   event initConsensusState(uint64 initHeight, bytes32 appHash);
@@ -75,17 +75,17 @@ contract TendermintLightClient is ILightClient, System, IParamSubscriber{
 
     //32 + 32 + 8 + 32 + 32 + cs.nextValidatorSet.length;
     uint256 length = 136 + cs.nextValidatorSet.length;
-    bytes memory input = new bytes(length+header.length);
+    bytes memory input = new bytes(length + header.length);
     uint256 ptr = Memory.dataPtr(input);
     require(encodeConsensusState(cs, preValidatorSetChangeHeight, ptr, length), "failed to serialize consensus state");
 
     // write header to input
     uint256 src;
-    ptr = ptr+length;
+    ptr = ptr + length;
     (src, length) = Memory.fromBytes(header);
     Memory.copy(src, ptr, length);
 
-    length = input.length+32;
+    length = input.length + 32;
     // Maximum validator quantity is 99
     bytes32[128] memory result;
     /* solium-disable-next-line */
@@ -103,11 +103,11 @@ contract TendermintLightClient is ILightClient, System, IParamSubscriber{
       length := mload(add(result, 0))
     }
     bool validatorChanged = false;
-    if ((length&(0x01<<248))!=0x00) {
+    if ((length & (0x01 << 248)) != 0x00) {
       validatorChanged = true;
       ISystemReward(SYSTEM_REWARD_ADDR).claimRewards(msg.sender, rewardForValidatorSetChange);
     }
-    length = length&0xffffffffffffffff;
+    length = length & 0xffffffffffffffff;
 
     /* solium-disable-next-line */
     assembly {
@@ -145,7 +145,7 @@ contract TendermintLightClient is ILightClient, System, IParamSubscriber{
   function getChainID() external view returns (string memory) {
     bytes memory chainIDBytes = new bytes(32);
     assembly {
-      mstore(add(chainIDBytes,32), sload(chainID_slot))
+      mstore(add(chainIDBytes, 32), sload(chainID_slot))
     }
 
     uint8 chainIDLength = 0;
@@ -175,36 +175,36 @@ contract TendermintLightClient is ILightClient, System, IParamSubscriber{
     uint256 length;
     (src, length) = Memory.fromBytes(cs.nextValidatorSet);
     Memory.copy(src, outputPtr, length);
-    outputPtr = outputPtr-32;
+    outputPtr = outputPtr - 32;
 
     bytes32 hash = cs.curValidatorSetHash;
     /* solium-disable-next-line */
     assembly {
       mstore(outputPtr, hash)
     }
-    outputPtr = outputPtr-32;
+    outputPtr = outputPtr - 32;
 
     hash = cs.appHash;
     /* solium-disable-next-line */
     assembly {
       mstore(outputPtr, hash)
     }
-    outputPtr = outputPtr-32;
+    outputPtr = outputPtr - 32;
 
     /* solium-disable-next-line */
     assembly {
       mstore(outputPtr, height)
     }
-    outputPtr = outputPtr-8;
+    outputPtr = outputPtr - 8;
 
     /* solium-disable-next-line */
     assembly {
       mstore(outputPtr, sload(chainID_slot))
     }
-    outputPtr = outputPtr-32;
+    outputPtr = outputPtr - 32;
 
     // size doesn't contain length
-    size = size-32;
+    size = size - 32;
     /* solium-disable-next-line */
     assembly {
       mstore(outputPtr, size)
@@ -216,22 +216,22 @@ contract TendermintLightClient is ILightClient, System, IParamSubscriber{
   // | chainID  | height   | appHash  | curValidatorSetHash | [{validator pubkey, voting power}] |
   // | 32 bytes  | 8 bytes  | 32 bytes | 32 bytes            | [{32 bytes, 8 bytes}]              |
   /* solium-disable-next-line */
-  function decodeConsensusState(uint256 ptr, uint256 size, bool leaveOutValidatorSet) internal pure returns(ConsensusState memory, uint64) {
-    ptr = ptr+8;
+  function decodeConsensusState(uint256 ptr, uint256 size, bool leaveOutValidatorSet) internal pure returns (ConsensusState memory, uint64) {
+    ptr = ptr + 8;
     uint64 height;
     /* solium-disable-next-line */
     assembly {
       height := mload(ptr)
     }
 
-    ptr = ptr+32;
+    ptr = ptr + 32;
     bytes32 appHash;
     /* solium-disable-next-line */
     assembly {
       appHash := mload(ptr)
     }
 
-    ptr = ptr+32;
+    ptr = ptr + 32;
     bytes32 curValidatorSetHash;
     /* solium-disable-next-line */
     assembly {
@@ -245,17 +245,17 @@ contract TendermintLightClient is ILightClient, System, IParamSubscriber{
     if (!leaveOutValidatorSet) {
       uint256 dest;
       uint256 length;
-      cs.nextValidatorSet = new bytes(size-104);
-      (dest,length) = Memory.fromBytes(cs.nextValidatorSet);
+      cs.nextValidatorSet = new bytes(size - 104);
+      (dest, length) = Memory.fromBytes(cs.nextValidatorSet);
 
-      Memory.copy(ptr+32, dest, length);
+      Memory.copy(ptr + 32, dest, length);
     }
 
     return (cs, height);
   }
 
-  function updateParam(string calldata key, bytes calldata value) external override onlyInit onlyGov{
-    if (Memory.compareStrings(key,"rewardForValidatorSetChange")) {
+  function updateParam(string calldata key, bytes calldata value) external override onlyInit onlyGov {
+    if (Memory.compareStrings(key, "rewardForValidatorSetChange")) {
       require(value.length == 32, "length of rewardForValidatorSetChange mismatch");
       uint256 newRewardForValidatorSetChange = BytesToTypes.bytesToUint256(32, value);
       require(newRewardForValidatorSetChange > 0 && newRewardForValidatorSetChange <= 1e18, "the newRewardForValidatorSetChange out of range");
