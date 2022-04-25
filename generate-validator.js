4dbe42426d69cb30227b1e0cb06f2388e0c27e9f
Loverush <luoshen1997@gmail.com>
2022-04-25 11:38:52 +0800

    Update for new fast finality rules and related issues

--- generate-validator.js
+++ generate-validator.js
@@ -1,35 +1,38 @@
 const fs = require("fs");
-const readline = require('readline');
+const readline = require("readline");
 const nunjucks = require("nunjucks");
-
+const BLSKeys = require("./BLSkeystore.json");
 
 async function processValidatorConf() {
-  const fileStream = fs.createReadStream(__dirname + '/validators.conf');
+  const fileStream = fs.createReadStream(__dirname + "/validators.conf");
+  const publicKey = BLSKeys.public_key;
 
   const rl = readline.createInterface({
     input: fileStream,
-    crlfDelay: Infinity
+    crlfDelay: Infinity,
   });
   let validators = [];
   for await (const line of rl) {
     // Each line in input.txt will be successively available here as `line`.
-    let vs = line.split(",")
+    let vs = line.split(",");
     validators.push({
       consensusAddr: vs[0],
       feeAddr: vs[1],
       bscFeeAddr: vs[2],
       votingPower: vs[3],
-    })
+      bLSPublicKey: "0x" + publicKey.pop()
+    });
   }
   return validators
 }
 
-processValidatorConf().then(function (validators) {
+processValidatorConf().then(function (validators, bLSPublicKeys) {
   const data = {
-    validators: validators
+    validators: validators,
+    bLSPublicKeys: bLSPublicKeys,
   };
-  const templateString = fs.readFileSync(__dirname + '/validators.template').toString();
+  const templateString = fs.readFileSync(__dirname + "/validators.template").toString();
   const resultString = nunjucks.renderString(templateString, data);
-  fs.writeFileSync(__dirname + '/validators.js', resultString);
+  fs.writeFileSync(__dirname + "/validators.js", resultString);
   console.log("BSCValidatorSet file updated.");
-})
+});
