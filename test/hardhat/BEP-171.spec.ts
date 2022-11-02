17ab9eacbd8c93a1943c07686376df7cb2d5019b
Ethan <cosinlink@163.com>
2022-11-02 14:56:26 +0800

    feat: add tests for cross-chain suspend

---
+++ test/hardhat/BEP-171.spec.ts
@@ -0,0 +1,342 @@
+import { BigNumber, Contract } from 'ethers';
+// @ts-ignore
+import { ethers } from 'hardhat';
+import {
+  deployContract,
+  waitTx,
+  setSlashIndicator,
+  validatorUpdateRlpEncode,
+  buildSyncPackagePrefix,
+  serializeGovPack,
+  mineBlocks,
+} from './helper';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import web3 from 'web3';
+import {
+  BSCValidatorSet,
+  SlashIndicator,
+  CrossChain,
+  GovHub,
+  RelayerHub,
+  RelayerIncentivize,
+  SystemReward,
+  TendermintLightClient,
+  Staking, TokenHub,
+} from '../../typechain-types';
+import { expect } from "chai";
+
+const log = console.log;
+const STAKE_CHANNEL_ID = 0x08;
+const GOV_CHANNEL_ID = 0x09;
+const proof = Buffer.from(web3.utils.hexToBytes('0x00'));
+const merkleHeight = 100;
+
+const deployContractAndInit = async (
+  deployer: SignerWithAddress,
+  factoryPath: string,
+  needInit?: boolean
+): Promise<Contract> => {
+  const instance = await deployContract(deployer, factoryPath);
+  if (needInit) {
+    await (await instance.init()).wait(1);
+  }
+  return instance;
+};
+
+describe('BEP-171 TEST', () => {
+  const unit = ethers.constants.WeiPerEther;
+  let instances: any[];
+
+  let tokenHub: TokenHub;
+  let relayerIncentivize: RelayerIncentivize;
+  let tendermintLightClient: TendermintLightClient;
+  let validatorSet: BSCValidatorSet;
+  let systemReward: SystemReward;
+  let slashIndicator: SlashIndicator;
+  let crosschain: CrossChain;
+  let relayerHub: RelayerHub;
+  let govHub: GovHub;
+  let staking: Staking;
+
+  let operator: SignerWithAddress;
+  let validators: string[];
+  let relayerAccount: string;
+  let signers: SignerWithAddress[];
+
+  before('before', async () => {
+    signers = await ethers.getSigners();
+    log(signers.length);
+    operator = signers[0];
+    relayerAccount = signers[1].address;
+    validators = signers.slice(0, 100).map((signer) => signer.address);
+
+    const contractPaths = [
+      {
+        name: 'RelayerIncentivize', // 0
+        needInit: true,
+        needUpdate: true,
+      },
+      {
+        name: 'TendermintLightClient', // 1
+        needInit: true,
+        needUpdate: true,
+      },
+      {
+        name: 'CrossChain', // 2
+        needInit: false,
+        needUpdate: true,
+      },
+      {
+        name: 'SystemReward', // 3
+        needInit: false,
+        needUpdate: false,
+      },
+      {
+        name: 'MockLightClient', // 4
+        needInit: false,
+        needUpdate: false,
+      },
+      {
+        name: 'TokenHub', // 5
+        needInit: true,
+        needUpdate: true,
+      },
+      {
+        name: 'TokenManager', // 6
+        needInit: false,
+        needUpdate: true,
+      },
+      {
+        name: 'RelayerHub', // 7
+        needInit: true,
+        needUpdate: true,
+      },
+      {
+        name: 'SlashIndicator', // 8
+        needInit: true,
+        needUpdate: true,
+      },
+      {
+        name: 'GovHub', // 9
+        needInit: false,
+        needUpdate: true,
+      },
+      {
+        name: 'BSCValidatorSet', // 10
+        needInit: true,
+        needUpdate: true,
+      },
+      {
+        name: 'Staking', // 11
+        needInit: false,
+        needUpdate: true,
+      },
+    ];
+    instances = [];
+    for (let i = 0; i < contractPaths.length; i++) {
+      const pathObj = contractPaths[i];
+      instances.push(await deployContractAndInit(operator, pathObj.name, pathObj.needInit));
+    }
+
+    for (let i = 0; i < contractPaths.length; i++) {
+      const pathObj = contractPaths[i];
+      const instance = instances[i];
+
+      if (!pathObj.needUpdate) {
+        continue;
+      }
+
+      let crosschainAddress = instances[2].address;
+      if (pathObj.name === 'BSCValidatorSet' || pathObj.name === 'GovHub') {
+        crosschainAddress = operator.address;
+      }
+
+      await waitTx(
+        instance.updateContractAddr(
+          instances[10].address,
+          instances[8].address,
+          instances[3].address,
+          instances[4].address,
+          instances[5].address,
+          instances[0].address,
+          instances[7].address,
+          instances[9].address,
+          instances[6].address,
+          crosschainAddress,
+          instances[11].address,
+        )
+      );
+    }
+
+    tokenHub = instances[5] as TokenHub
+
+    relayerIncentivize = instances[0];
+    tendermintLightClient = instances[1];
+    systemReward = instances[3] as SystemReward;
+    slashIndicator = instances[8] as SlashIndicator;
+    await waitTx(systemReward.addOperator(operator.address));
+    await waitTx(systemReward.addOperator(tendermintLightClient.address));
+    await waitTx(systemReward.addOperator(relayerIncentivize.address));
+
+    validatorSet = instances[10] as BSCValidatorSet;
+    relayerHub = instances[7] as RelayerHub;
+
+    crosschain = instances[2] as CrossChain;
+    await waitTx(crosschain.init());
+
+    govHub = instances[9] as GovHub;
+
+    staking = instances[11] as Staking;
+  });
+
+  beforeEach('beforeEach', async () => {});
+
+  it('query code size', async () => {
+    let code = await ethers.provider.getCode(crosschain.address)
+    let codeSize = (code.length - 2) / 2
+    log(`CrossChain Mock Template code size: ${codeSize}, UpperLimit: 24567` )
+
+    code = await ethers.provider.getCode(tokenHub.address)
+    codeSize = (code.length - 2) / 2
+    log(`TokenHub Mock Template code size: ${codeSize}, UpperLimit: 24567` )
+  });
+
+  it('update validators', async () => {
+    // do update validators
+    let packageBytes = validatorUpdateRlpEncode(
+      validators.slice(1, 22),
+      validators.slice(1, 22),
+      validators.slice(1, 22)
+    );
+
+    await waitTx(validatorSet.connect(operator).handleSynPackage(STAKE_CHANNEL_ID, packageBytes));
+  });
+
+  it('query all view func', async () => {
+    expect(await validatorSet.getValidators()).to.deep.eq(validators.slice(1, 22));
+    for (let i = 1; i < 50; i++) {
+      const currentValidatorSetIndex = i - 1
+      if (i >= 1 && i < 22) {
+        expect(await validatorSet.isWorkingValidator(currentValidatorSetIndex)).to.deep.eq(true);
+        expect(await validatorSet.isCurrentValidator(validators[i])).to.deep.eq(true);
+        expect(await validatorSet.canEnterMaintenance(validators[i])).to.deep.eq(false);
+      } else {
+        expect(await validatorSet.isWorkingValidator(currentValidatorSetIndex)).to.be.eq(false);
+        expect(await validatorSet.isCurrentValidator(validators[i])).to.deep.eq(false);
+        expect(await validatorSet.canEnterMaintenance(validators[i])).to.deep.eq(false);
+      }
+      expect(await validatorSet.getIncoming(validators[i])).to.deep.eq(0);
+    }
+
+    expect(await validatorSet.getMaintainingValidators()).to.deep.eq([]);
+    expect(await validatorSet.numOfMaintaining()).to.be.eq(0);
+
+  });
+
+  it('common case 1-1 update params', async () => {
+    await waitTx(relayerHub.connect(operator).register({ value: unit.mul(100) }));
+    await waitTx(
+      govHub.updateContractAddr(
+        instances[10].address,
+        instances[8].address,
+        instances[3].address,
+        instances[4].address,
+        instances[5].address,
+        instances[0].address,
+        instances[7].address,
+        instances[9].address,
+        instances[6].address,
+        instances[2].address,
+        instances[11].address,
+      )
+    );
+
+    //  set maxNumOfMaintaining to 5
+    let govChannelSeq = await crosschain.channelReceiveSequenceMap(GOV_CHANNEL_ID);
+    let govValue = '0x0000000000000000000000000000000000000000000000000000000000000005'; // 5;
+    let govPackageBytes = serializeGovPack('maxNumOfMaintaining', govValue, validatorSet.address);
+    await crosschain
+      .connect(operator)
+      .handlePackage(
+        Buffer.concat([buildSyncPackagePrefix(2e16), govPackageBytes]),
+        proof,
+        merkleHeight,
+        govChannelSeq,
+        GOV_CHANNEL_ID
+      );
+    expect(await validatorSet.maxNumOfMaintaining()).to.be.eq(BigNumber.from(govValue));
+
+    //  set maintainSlashScale to 2
+    govChannelSeq = await crosschain.channelReceiveSequenceMap(GOV_CHANNEL_ID);
+    govValue = '0x0000000000000000000000000000000000000000000000000000000000000002'; // 2;
+    govPackageBytes = serializeGovPack('maintainSlashScale', govValue, validatorSet.address);
+    await crosschain
+      .connect(operator)
+      .handlePackage(
+        Buffer.concat([buildSyncPackagePrefix(2e16), govPackageBytes]),
+        proof,
+        merkleHeight,
+        govChannelSeq,
+        GOV_CHANNEL_ID
+      );
+  });
+
+  it('suspend fail, not cabinet', async () => {
+    expect(crosschain.connect(signers[22]).suspend()).to.be.revertedWith("not cabinet");
+  })
+
+  it('suspend success, all cross-chain channels closed', async () => {
+    await waitTx(crosschain.connect(signers[1]).suspend());
+    expect(await crosschain.isSuspended()).to.be.eq(true);
+
+    let govChannelSeq = await crosschain.channelReceiveSequenceMap(GOV_CHANNEL_ID);
+    let govValue = '0x0000000000000000000000000000000000000000000000000000000000000005'; // 5;
+    let govPackageBytes = serializeGovPack('maxNumOfMaintaining', govValue, validatorSet.address);
+
+
+    expect(crosschain.connect(operator).handlePackage(
+        Buffer.concat([buildSyncPackagePrefix(2e16), govPackageBytes]),
+        proof,
+        merkleHeight,
+        govChannelSeq,
+        GOV_CHANNEL_ID
+      // @ts-ignore
+    )).to.be.revertedWith("suspended")
+    expect(crosschain.connect(operator).handlePackage(
+        Buffer.concat([buildSyncPackagePrefix(2e16), govPackageBytes]),
+        proof,
+        merkleHeight,
+        await crosschain.channelReceiveSequenceMap(STAKE_CHANNEL_ID),
+        STAKE_CHANNEL_ID
+      // @ts-ignore
+    )).to.be.revertedWith("suspended")
+  });
+
+  it('suspend fail, already suspended', async () => {
+    expect(await crosschain.isSuspended()).to.be.eq(true);
+    expect(crosschain.connect(signers[1]).suspend()).to.be.revertedWith("suspended")
+  });
+
+  it('reopen success, cross-chain works', async () => {
+    await waitTx(crosschain.connect(signers[2]).reopen());
+    expect(await crosschain.isSuspended()).to.be.eq(true);
+
+    expect(crosschain.connect(signers[2]).reopen()).to.be.revertedWith("already approved");
+    expect(await crosschain.isSuspended()).to.be.eq(true);
+
+    await waitTx(crosschain.connect(signers[3]).reopen());
+    expect(await crosschain.isSuspended()).to.be.eq(false);
+
+    let govChannelSeq = await crosschain.channelReceiveSequenceMap(GOV_CHANNEL_ID);
+    let govValue = '0x0000000000000000000000000000000000000000000000000000000000000007'; // 7;
+    let govPackageBytes = serializeGovPack('maxNumOfMaintaining', govValue, validatorSet.address);
+    await waitTx(crosschain.connect(operator).handlePackage(
+        Buffer.concat([buildSyncPackagePrefix(2e16), govPackageBytes]),
+        proof,
+        merkleHeight,
+        govChannelSeq,
+        GOV_CHANNEL_ID
+    ))
+    expect(await validatorSet.maxNumOfMaintaining()).to.be.eq(BigNumber.from(govValue));
+  });
+});
