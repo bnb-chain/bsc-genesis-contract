479590b5a64e9627fc7100ea9fcb77808e58a1ba
Roshan <luoshen1997@gmail.com>
2022-12-13 17:54:19 +0800

    delete unused mock contracts interface

--- lib/interface/IMockRelayerHub.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.8.10;
-
-interface MockRelayerHub {
-    function isRelayer(address) external view returns (bool);
-}
--- lib/interface/IMockSystemReward.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.10;
-
-interface MockSystemReward {
-    event RewardEmpty();
-    event RewardTo(address indexed to, uint256 indexed amount);
-
-    function claimRewards(address to, uint256 amount) external payable;
-}
