eeca2091bf0944f18f16cd5dc3df058a3f306b5d
larry.lx <larry.lx@outlook.com>
2023-06-30 14:07:02 +0800

    docs: update the bnbchain urls

--- contracts/interface/ICrossChain.sol
+++ contracts/interface/ICrossChain.sol
@@ -2,7 +2,7 @@ pragma solidity 0.6.4;
 
 interface ICrossChain {
     /**
-     * @dev Send package to Binance Chain
+     * @dev Send package to BNB Beacon Chain
      */
     function sendSynPackage(uint8 channelId, bytes calldata msgBytes, uint256 relayFee) external;
 }
