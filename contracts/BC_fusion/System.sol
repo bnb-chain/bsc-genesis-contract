12f035e47a978b2e0819c36a8c5133e73e78dd5e
Roshan <luoshen1997@gmail.com>
2023-08-28 17:53:45 +0800

    fix build error

---
+++ contracts/BC_fusion/System.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+contract System {
+
+    bool public alreadyInit;
+
+    uint32 public constant CODE_OK = 0;
+    uint32 public constant ERROR_FAIL_DECODE = 100;
+
+    uint8 constant public BIND_CHANNELID = 0x01;
+    uint8 constant public TRANSFER_IN_CHANNELID = 0x02;
+    uint8 constant public TRANSFER_OUT_CHANNELID = 0x03;
+    uint8 constant public STAKING_CHANNELID = 0x08;
+    uint8 constant public GOV_CHANNELID = 0x09;
+    uint8 constant public SLASH_CHANNELID = 0x0b;
+    uint8 constant public CROSS_STAKE_CHANNELID = 0x10;
+    uint16 constant public bscChainID = 0x0060;
+
+    address public constant VALIDATOR_CONTRACT_ADDR = 0x0000000000000000000000000000000000001000;
+    address public constant SLASH_CONTRACT_ADDR = 0x0000000000000000000000000000000000001001;
+    address public constant SYSTEM_REWARD_ADDR = 0x0000000000000000000000000000000000001002;
+    address public constant LIGHT_CLIENT_ADDR = 0x0000000000000000000000000000000000001003;
+    address public constant TOKEN_HUB_ADDR = 0x0000000000000000000000000000000000001004;
+    address public constant INCENTIVIZE_ADDR=0x0000000000000000000000000000000000001005;
+    address public constant RELAYERHUB_CONTRACT_ADDR = 0x0000000000000000000000000000000000001006;
+    address public constant GOV_HUB_ADDR = 0x0000000000000000000000000000000000001007;
+    address public constant TOKEN_MANAGER_ADDR = 0x0000000000000000000000000000000000001008;
+    address public constant CROSS_CHAIN_CONTRACT_ADDR = 0x0000000000000000000000000000000000002000;
+    address public constant STAKING_CONTRACT_ADDR = 0x0000000000000000000000000000000000002001;
+    address public constant STAKE_HUB_ADDR = 0x0000000000000000000000000000000000002002;
+
+    modifier onlyCoinbase() {
+        require(msg.sender == block.coinbase, "the message sender must be the block producer");
+        _;
+    }
+
+    modifier onlyNotInit() {
+        require(!alreadyInit, "the contract already init");
+        _;
+    }
+
+    modifier onlyInit() {
+        require(alreadyInit, "the contract not init yet");
+        _;
+    }
+
+    modifier onlySlash() {
+        require(msg.sender == SLASH_CONTRACT_ADDR, "the message sender must be slash contract");
+        _;
+    }
+
+    modifier onlyTokenHub() {
+        require(msg.sender == TOKEN_HUB_ADDR, "the message sender must be token hub contract");
+        _;
+    }
+
+    modifier onlyGov() {
+        require(msg.sender == GOV_HUB_ADDR, "the message sender must be governance contract");
+        _;
+    }
+
+    modifier onlyValidatorContract() {
+        require(msg.sender == VALIDATOR_CONTRACT_ADDR, "the message sender must be validatorSet contract");
+        _;
+    }
+
+    modifier onlyCrossChainContract() {
+        require(msg.sender == CROSS_CHAIN_CONTRACT_ADDR, "the message sender must be cross chain contract");
+        _;
+    }
+
+    modifier onlyRelayerIncentivize() {
+        require(msg.sender == INCENTIVIZE_ADDR, "the message sender must be incentivize contract");
+        _;
+    }
+
+    modifier onlyTokenManager() {
+        require(msg.sender == TOKEN_MANAGER_ADDR, "the msg sender must be tokenManager");
+        _;
+    }
+
+    modifier onlyStakeHub() {
+        require(msg.sender == STAKE_HUB_ADDR, "the msg sender must be stakeHub");
+        _;
+    }
+
+    // Not reliable, do not use when need strong verify
+    function isContract(address addr) internal view returns (bool) {
+        uint size;
+        assembly { size := extcodesize(addr) }
+        return size > 0;
+    }
+}
