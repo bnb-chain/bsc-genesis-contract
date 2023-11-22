4c6d2fd63c601e64784c7eac6e55eb340450fd72
buddho <galaxystroller@gmail.com>
2023-11-22 18:04:33 +0800

    init SystemReward for local test and add shanghaiTime,keplerTime (#422)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -9,7 +9,7 @@ function generate_local() {
     bash ${basedir}/generate-system.sh --bscChainId 02ca
     bash ${basedir}/generate-crosschain.sh --bscChainId 02ca
     bash ${basedir}/generate-relayerHub.sh --network local
-    bash ${basedir}/generate-tendermintLightClient.sh --initConsensusStateBytes "42696e616e63652d436861696e2d4e696c650000000000000000000000000000000000000000000229eca254b3859bffefaf85f4c95da9fbd26527766b784272789c30ec56b380b6eb96442aaab207bc59978ba3dd477690f5c5872334fc39e627723daa97e441e88ba4515150ec3182bc82593df36f8abb25a619187fcfab7e552b94e64ed2deed000000e8d4a51000"
+    bash ${basedir}/generate-tendermintLightClient.sh --network local --initConsensusStateBytes "42696e616e63652d436861696e2d4e696c650000000000000000000000000000000000000000000229eca254b3859bffefaf85f4c95da9fbd26527766b784272789c30ec56b380b6eb96442aaab207bc59978ba3dd477690f5c5872334fc39e627723daa97e441e88ba4515150ec3182bc82593df36f8abb25a619187fcfab7e552b94e64ed2deed000000e8d4a51000"
     bash ${basedir}/generate-validatorSet.sh --initBurnRatio 1000 --network local
     bash ${basedir}/generate-systemReward.sh --network local
     bash ${basedir}/generate-slashIndicator.sh --network local
