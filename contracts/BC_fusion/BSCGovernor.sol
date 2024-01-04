46c522b98e5e31f15af33847163e72357a51f70b
Ethan <cosinlinker@gmail.com>
2024-01-04 19:33:57 +0800

    feat: add whenNotPaused for queue in BSCGovernor (#456)
    * feat: add whenNotPaused for queue in BSCGovernor
    
    * chores: lint code
    
    * feat: add comments
    
    * fix: lint

--- contracts/BC_fusion/BSCGovernor.sol
+++ contracts/BC_fusion/BSCGovernor.sol
@@ -135,6 +135,27 @@ contract BSCGovernor is
     }
 
     /**
+     * @dev Function to queue a proposal to the timelock.
+     * @param targets target contracts to call
+     * @param values msg.value for each contract call
+     * @param calldatas calldata for each contract call
+     * @param descriptionHash the description hash
+     */
+    function queue(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    )
+        public
+        override(GovernorTimelockControlUpgradeable, IGovernorTimelockUpgradeable)
+        whenNotPaused
+        returns (uint256 proposalId)
+    {
+        return GovernorTimelockControlUpgradeable.queue(targets, values, calldatas, descriptionHash);
+    }
+
+    /**
      * @dev Cancel a proposal. A proposal is cancellable by the proposer, but only while it is Pending state, i.e.
      * before the vote starts.
      *
