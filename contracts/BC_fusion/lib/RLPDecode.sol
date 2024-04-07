d53aacd9643e2ff20c747019fbcfe0c0cf8d271f
alwayshang <zhanghonghao@outlook.com>
2024-04-07 14:08:35 +0800

    chore: fix some typos
    Signed-off-by: alwayshang <zhanghonghao@outlook.com>

--- contracts/BC_fusion/lib/RLPDecode.sol
+++ contracts/BC_fusion/lib/RLPDecode.sol
@@ -144,7 +144,7 @@ library RLPDecode {
         assembly {
             result := mload(memPtr)
 
-            // shfit to the correct location if neccesary
+            // shfit to the correct location if necessary
             if lt(len, 32) { result := div(result, exp(256, sub(32, len))) }
         }
 
